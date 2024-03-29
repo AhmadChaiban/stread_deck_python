df58ba2d57f64cceb85a0437425dee9b3c4b4dc1
Ahmad Chaiban <Ahmadchaiban@gmail.com>
2024-03-29 00:48:12 -0400

    translator script edit

---
+++ translator.py
@@ -0,0 +1,27 @@
+import os
+from AppKit import NSPasteboard, NSStringPboardType
+import pyperclip as pc
+from googletrans import Translator
+
+def notify(title, text):
+    os.system(f"osascript -e 'display notification \"{title}\" with title \"{text}\"'")
+
+
+
+pb = NSPasteboard.generalPasteboard()
+pbstring = pb.stringForType_(NSStringPboardType)
+
+translator = Translator()
+translation = translator.translate(pbstring).text
+
+applescript = """
+display dialog "
+""" + translation + """" ¬
+with title "This is a pop-up window" ¬
+with icon caution ¬
+buttons {"OK"}
+"""
+
+# os.system(f"osascript -e '{applescript}'")
+
+pc.copy(translation)
